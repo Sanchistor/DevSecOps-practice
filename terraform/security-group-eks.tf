82432d1792e7909b670c7d58f9fbba3d2304ecfd
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-08 15:55:35 +0200

    Configure EKS 3

--- terraform/security-group-eks.tf
+++ terraform/security-group-eks.tf
@@ -1,53 +1,85 @@
 resource "aws_security_group" "eks_control_plane_sg" {
   name        = "${var.cluster_name}-control-plane"
-  description = "Security group for the EKS control plane"
+  description = "Security group for control plane"
   vpc_id      = data.aws_vpc.default.id
 
-  # Allow inbound traffic from worker nodes to API server (port 6443)
   ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    security_groups = [aws_security_group.eks_worker_node_sg.id]
+    cidr_blocks = ["172.19.0.0/24"]
   }
 
-  # Allow outbound traffic to worker nodes
   egress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.19.0.0/24"]
   }
-}
 
-
-resource "aws_security_group" "eks_worker_node_sg" {
-  name        = "${var.cluster_name}-worker-node"
-  description = "Security group for worker nodes"
-  vpc_id      = data.aws_vpc.default.id
-
-  # Allow worker nodes to receive communication from the control plane (port 10250)
   ingress {
     from_port   = 10250
     to_port     = 10250
     protocol    = "tcp"
-    security_groups = [aws_security_group.eks_control_plane_sg.id]
+    cidr_blocks = ["172.19.0.0/24"]
   }
 
-  # ✅ Allow nodes to communicate with each other using `self`
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    self        = true
-  }
-
-  # Allow all outbound traffic
   egress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 10250
+    to_port     = 10250
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.19.0.0/24"]
   }
+
 }
 
+resource "aws_security_group" "eks_worker_node_sg" {
+
+  name        = "${var.cluster_name}-worker-node"
+  description = "Security group for control plane"
+  vpc_id      = data.aws_vpc.default.id
+
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+  egress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+  ingress {
+    from_port   = 10256
+    to_port     = 10256
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+  egress {
+    from_port   = 10256
+    to_port     = 10256
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+  egress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["172.19.0.0/24"]
+  }
+
+}
