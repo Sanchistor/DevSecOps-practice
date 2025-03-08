e84a40e8eeae2f7e553e9a6947a4686c57000ef9
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-08 15:57:46 +0200

    Configure EKS 4

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -27,3 +27,15 @@ module "eks" {
     }
   }
 }
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
