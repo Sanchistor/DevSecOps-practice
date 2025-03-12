7f68cfe3ac0371de9e0e3437229ce04063546941
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-12 14:32:41 +0200

    Fix rds sg

--- terraform/security-group-rds.tf
+++ terraform/security-group-rds.tf
@@ -3,19 +3,19 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_security_group" "rds" {
-  vpc_id      = data.aws_vpc.default.id
-  name = "rds-sg"
+  vpc_id = data.aws_vpc.default.id
+  name   = "rds-sg"
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.subnet_cidrs
   }
 
   egress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.subnet_cidrs
   }
 }
