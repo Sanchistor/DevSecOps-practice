e2db39b20eac60c8af87f029c4f185d8c696c202
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-24 13:22:47 +0200

    Fix connection to RDS

--- terraform/security-group-rds.tf
+++ terraform/security-group-rds.tf
@@ -12,6 +12,13 @@ resource "aws_security_group" "rds" {
     cidr_blocks = var.subnet_cidrs
   }
 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["212.142.114.55/32"]
+  }
+
   egress {
     from_port   = 5432
     to_port     = 5432
