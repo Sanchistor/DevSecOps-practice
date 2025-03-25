def1fbec9b4f656a56f75391b326c1d506eb54a2
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-25 16:49:24 +0200

    Fix migrations 2

--- home/models.py
+++ home/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
     # Intentionally weak model: no encryption, no validation
-    title = models.CharField(max_length=255)
-    content = models.TextField()  # No input sanitization
-    user_password = models.CharField(max_length=255)  # Plaintext password (don't do this in production)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('title'),
-        FieldPanel('content'),
-        FieldPanel('user_password'),  # Insecure way of storing passwords
-    ]
+    content = models.TextField(default="defaultconstent")  # No input sanitization
+    user_password = models.CharField(max_length=255, default="defaultpassword")  # Plaintext password (don't do this in production)
     
 #Vulnerabilty of Insecure Password Storage
 class User(models.Model):
     username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, default="defaultpassword")
