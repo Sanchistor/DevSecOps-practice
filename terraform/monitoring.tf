2dd720a76a10b1e0f8eb2021679e85ad8fd352fb
Oleksandr Zadyraka <sashazadiraka@gmail.com>
2025-03-28 14:38:09 +0200

    Fix monitoring parameters 2

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_dashboard" "security_dashboard" {
         "height" = 6,
         "properties" = {
           "metrics" = [
-            ["Security", "SAST_Vulnerabilities", "Build"]
+            [{ "expression" : "SEARCH('{Security,SAST_Vulnerabilities,Build} MetricName=\"SAST_Vulnerabilities\"', 'Sum', 300)" }]
           ],
           "title"  = "SAST Vulnerabilities per Build",
           "stat"   = "Sum",
@@ -28,24 +28,7 @@ resource "aws_cloudwatch_dashboard" "security_dashboard" {
         "height" = 6,
         "properties" = {
           "metrics" = [
-            ["Security", "DAST_Vulnerabilities", "Build"]
-          ],
-          "title"  = "DAST Vulnerabilities per Build",
-          "stat"   = "Sum",
-          "region" = var.region,
-          "period" = 300,
-          "view"   = "timeSeries"
-        }
-      },
-      {
-        "type"   = "metric",
-        "x"      = 0,
-        "y"      = 12,
-        "width"  = 12,
-        "height" = 6,
-        "properties" = {
-          "metrics" = [
-            ["Security", "DepScan_Vulnerabilities", "Build"]
+            [{ "expression" : "SEARCH('{Security,DepScan_Vulnerabilities,Build} MetricName=\"DepScan_Vulnerabilities\"', 'Sum', 300)" }]
           ],
           "title"  = "Dependency Scan Vulnerabilities per Build",
           "stat"   = "Sum",
